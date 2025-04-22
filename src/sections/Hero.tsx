64adaa293d0f802212ebda8a0a2c4eda79a9b321
kanai <kanai@kanais-Air.lan>
2025-04-23 01:16:22 +0530

    started hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,34 @@
+import meImage from '@/assets/images/me.jpg';
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className='py-32'>
+      <div className="container">
+        <Image 
+        src={meImage}
+        className="size-[100px]"
+        alt="My image" />
+        <div>
+          <div></div>
+          <div>
+            Available for new projects
+          </div>
+          <h1>Building exceptional user experiences</h1>
+          <p>I build websites and web apps</p>
+          <div>
+            <button>
+              <span>Explore My Works</span>
+              <ArrowDown />
+            </button>
+            <button>
+              <span>👋🏻</span>
+              <span>Let's Connect</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
