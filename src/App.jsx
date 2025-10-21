b7496c57b53918625b10f8e552a32687aeae3d23
kunstewi <kan.ai35423@gmail.com>
2025-10-21 21:39:16 +0530

    initialised project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <div>
+        <Navbar />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { motion, useScroll, AnimatePresence } from "framer-motion";
+import { Code2, Sun, Moon, Menu, X } from "lucide-react";
+import { useTheme } from "../context/ThemeContext";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
